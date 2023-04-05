20eb8493bc61e9fb7a6e9d33423aaa8636200910
svitlana.oliinyk <101010916+oliinyks@users.noreply.github.com>
2023-04-05 19:51:49 +0300

    add theme

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,36 @@ import PostIcon from "@mui/icons-material/Book";
 import UserIcon from "@mui/icons-material/Group";
 import { Dashboard } from "./Dashboard";
 import { authProvider } from "./authProvider";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { createTheme } from '@mui/material/styles';
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 
-const App = () => (
-  <Admin
-    authProvider={authProvider}
-    dataProvider={dataProvider}
-    dashboard={Dashboard}
-  >
-    <Resource name="posts" list={PostList} edit={PostEdit} icon={PostIcon} />
-    <Resource
-      name="users"
-      list={UserList}
-      recordRepresentation="name"
-      create={PostCreate}
-      icon={UserIcon}
-    />
-  </Admin>
-);
+function App() {
+	const theme = useMemo(() => createTheme(themeSettings), []);
+	return (
+      <Admin
+        theme={theme}
+        authProvider={authProvider}
+        dataProvider={dataProvider}
+        dashboard={Dashboard}
+      >
+        <Resource
+          name="posts"
+          list={PostList}
+          edit={PostEdit}
+          icon={PostIcon}
+        />
+        <Resource
+          name="users"
+          list={UserList}
+          recordRepresentation="name"
+          create={PostCreate}
+          icon={UserIcon}
+        />
+      </Admin>
+  );
+};
 
 export default App;
